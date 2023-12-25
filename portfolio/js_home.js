73119de0a81f9644e020c5ed76e2719d4e9a665b
raissi-hosni <hosnyrs73@gmail.com>
2023-12-25 20:12:45 +0100

    A simple portfolio with Html, Css, and Js.

---
+++ portfolio/js_home.js
@@ -0,0 +1,12 @@
+
+
+
+
+function user(){
+    let first_name=document.getElementById("first_name").value;
+    let last_name=document.getElementById("last_name").value;
+    let e_mail=document.getElementById("e_mail").value;
+    let msg=document.getElementById("msg").value;
+    window.alert("thank you "+first_name+" "+last_name+" !");
+}
+
